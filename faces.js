693fb40d234cf3addd571f80ebcef73d9354b4fc
evan bates <evan.bates@rutgers.edu>
2015-11-12 15:29:46 -0500

    early beta

---
+++ faces.js
@@ -0,0 +1,16 @@
+var counter = 0;
+function countup() {
+  counter = counter + 1;
+  
+}
+
+function countdown() {
+  setTimeout(function countandgive()
+    { 
+    alert("Time is up, you clicked " +counter+ " SNL Heads!");
+     
+     }, 20000);
+
+}
+
+
---
+++ index.php
@@ -0,0 +1,1 @@
+<?php require 'faces.html' ;?>
